abd1e023356b134852df64c5e1252710ddbd32bb
unknown <aastikvyas28@gmail.com>
2024-04-16 17:22:09 +0530

    NewSeries

---
+++ index.js
@@ -0,0 +1,45 @@
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req ,res)=>{
+ res.send('You are on twitter')
+})
+
+app.get('/youtube',(req , res)=>{
+ res.send('You are on youtube')
+})
+
+app.get('/login' ,(req , res)=>{
+ res.send('<h1>YOU ARE ON DATABASE</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
